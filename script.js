d6761260adf699f96581b483b7764cb0023230c1
unknown <gaurav kuamr>
2019-05-19 02:17:09 +0530

    btn click mesage updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var btn = document.querySelector("#btn")
 var h1 = document.querySelector("h1")
  btn.addEventListener("click",function(){
- 	h1.innerHTML="Bye..!! to Github"
+ 	h1.innerHTML="This Time no Buy..!! to Github"
  })
